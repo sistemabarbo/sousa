6a4823da91d625676d1f009a3074e0e30d4f7701
sistemabarbo <132204097+sistemabarbo@users.noreply.github.com>
2024-04-11 12:16:45 -0300

    Update Grid.jsx

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -26,8 +26,13 @@ const Grid = ({ users, setUsers, setOnEdit }) => {
  <thead>
     <tr>
         <th>Nome</th>
+         <th>Departamento</th>
+         <th>Cpf</th>
+         <th>Email</th>
         <th>Telefone</th>
-        <th>Email</th>
+        <th>Cidade</th>
+         <th>Estado</th>
+         <th>Unidade</th>
         <th>Editar</th>
         <th>Remover</th>
     </tr>
@@ -36,8 +41,13 @@ const Grid = ({ users, setUsers, setOnEdit }) => {
     {users.map((item, i) => (
         <tr key={i}>
             <td width="30%">{item.nome}</td>
-            <td width="30%">{item.telefone}</td>
-            <td width="30%">{item.email}</td>
+             <td width="30%">{item.departamento}</td>
+             <td width="30%">{item.cpf}</td>
+             <td width="30%">{item.email}</td>
+             <td width="30%">{item.telefone}</td>
+            <td width="30%">{item.cidade}</td>
+            <td width="30%">{item.estado}</td>
+             <td width="30%">{item.unidade}</td>
             <td><button className="edit" onClick={() => handleEdit(item)}>Editar</button></td>
             <td><button className="delete" onClick={() => handleDelete(item.id)}>Deletar</button></td> 
         
